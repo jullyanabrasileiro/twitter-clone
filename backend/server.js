bd106038217c9af29e345515685299440b1b3b46
Jullyana <jullyana.brasileiro@hotmail.com>
2024-11-22 12:51:04 -0800

    mongodb connection

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,18 @@
 import express from "express";
+import dotenv from "dotenv";
+
 import authRoutes from "./routes/auth.routes.js";
+import { connect } from "mongoose";
+
+dotenv.config();
 
 const app = express();
+
+console.log(process.env.MONGO_URI);
 
 app.use("/api/auth", authRoutes);
 
 app.listen(8000, () => {
     console.log("server is runing on this port 8000");
+    connectMongoDB();
 });
