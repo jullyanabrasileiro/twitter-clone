3d4a4e6e0733d0d08812a810b32b74b06739ec6a
Jullyana <jullyana.brasileiro@hotmail.com>
2024-11-23 13:57:46 -0800

    models settings

---
+++ backend/models/user.model.js
@@ -0,0 +1,28 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema({
+    username: {
+        type: String,
+        required: true,
+        unique: true,
+    },
+    fullName: {
+        type: String,
+        required: true,
+    },
+    password: {
+        type: String,
+        required: true,
+        minLength: 6,
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+    },
+    followers : [
+        {
+            type: mongoose.Schema.Types.ObjectId,
+        }
+    ]
+}, {timestamps: true})
