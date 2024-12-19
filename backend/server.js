7797f5b9491497d8655564a939880b59cdd05548
Jullyana <jullyana.brasileiro@hotmail.com>
2024-12-19 15:38:46 -0800

    user controller

--- backend/server.js
+++ backend/server.js
@@ -2,7 +2,9 @@ import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
 
-import authRoutes from "./routes/auth.routes.js";
+import authRoutes from "./routes/auth.routes.js"
+import userRoutes from "./routes/user.routes.js"
+
 import connectMongoDB from "./db/connectMongoDB.js"
 
 dotenv.config();
@@ -16,6 +18,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
 app.use("/api/auth", authRoutes);
+app.use("/api/auth", userRoutes);
 
 app.listen(PORT, () => {
     console.log(`server is runing on this port  ${PORT}`);
