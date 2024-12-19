6740d1736ef5c85e2bf55b72122d1ec4c6c9112a
Jullyana <jullyana.brasileiro@hotmail.com>
2024-12-19 15:43:14 -0800

    followUnfollowUser wip

--- backend/routes/user.routes.js
+++ backend/routes/user.routes.js
@@ -4,9 +4,9 @@ import { getUserProfile } from '../controllers/user.controller.js';
 
 const router = express.Router();
 
-router.get("/profile/:username", protectRoute, getUserProfile)
+router.get("/profile/:username", protectRoute, getUserProfile);
 // router.get("/suggested", protectRoute, getUserProfile)
-// router.post("/follow/:id", protectRoute, followUnfollowUser)
+router.post("/follow/:id", protectRoute, followUnfollowUser);
 // router.post("/update", protectRoute, updateUserProfile)
 
 
