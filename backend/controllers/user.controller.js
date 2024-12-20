afe03ae84890441344059d5f58d47d710367d9a5
Jullyana <jullyana.brasileiro@hotmail.com>
2024-12-20 00:39:00 -0800

    unfollow the user features

--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -18,6 +18,30 @@ export const getUserProfile = async (req, res) => {
 export const followUnfollowUser = async (req, res) => {
     try {
         const { id } = req.params;
+        const userToModify = await User.findById(id);
+        const currentUser = await User.findById(req.user._id);
+
+        if(id === req.user._id) {
+            return res.status(400).json({ error: "You can't follow/unfollow yourself"})
+        };
+
+        if (!userToModify || !currentUser) return res.status(400).json({ error: "User not found" });
+
+        const isFollowing = currentUser.following.includes(id);
+
+        if (isFollowing) {
+            //unfollow the user
+            await User.findByIdAndUpdate(id, { $pull: { followers: req.user._id} });    
+            await User.findByIdAndUpdate(req.user._id, { $pull: { following: id }});      
+            res.status(200).json({ message: "User unfollowed successfully" });  
+
+        } else {
+            //follow the user
+            await User.findByIdAndUpdate(id, { $push: { followers: req.user._id} });
+            await User.findByIdAndUpdate(req.user._id, { $push: { following: id }});
+            //send notification to the user
+        }
+         
     } catch (error) {
         console.log("Error in followUnfollowUser: ", error.message);
         res.status(500).json({ error:error.message });
