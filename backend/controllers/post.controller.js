34656e59615ac47a68ca8f3ead30f03761700fda
Jullyana <jullyana.brasileiro@hotmail.com>
2025-01-08 23:38:17 -0800

    post controller features

---
+++ backend/controllers/post.controller.js
@@ -0,0 +1,8 @@
+export const createPost = async (req, res) => {
+    try {
+        const { text } = req.body;
+        let { img } = req.body;
+    } catch (error) {
+
+    }
+}
--- backend/routes/post.routes.js
+++ backend/routes/post.routes.js
@@ -1,12 +1,13 @@
 import express from 'express';
 import { protectRoute } from '../middleware/protectRoute';
+import { createPost } from '../controllers/post.controller';
 
 
 const router = express.Router();
 
 router.post("/create", protectRoute, createPost)
-router.post("/like/:id", protectRoute, likeUnlikePost)
-router.post("/comment", protectRoute, createPost)
-router.delete("/", protectRoute, deletePost)
+// router.post("/like/:id", protectRoute, likeUnlikePost)
+// router.post("/comment/:id", protectRoute, commentOnPost)
+// router.delete("/", protectRoute, deletePost)
 
 export default router;
